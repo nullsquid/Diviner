53285a51d8e79e509acf1b6babc66c018722bc47
Azrael <j.shepard.ramsay@gmail.com>
2015-11-01 08:38:30 -0500

    changed sacrifice mechanic, added ambient audio

---
+++ Assets/SCRIPTS/RUNTIME/ConnectToPivot.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConnectToPivot : MonoBehaviour {
+
+    DistanceJoint2D distanceJoint;
+    GameObject connectedBody;
+    void Awake() {
+        connectedBody = GameObject.Find("INTESTINE_PIVOT");
+        {
+            distanceJoint = gameObject.GetComponent<DistanceJoint2D>();
+            if (distanceJoint.enabled) {
+                distanceJoint.connectedBody = connectedBody.GetComponent<Rigidbody2D>();
+
+            }
+
+        }
+
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
