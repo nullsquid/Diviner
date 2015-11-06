6eb1670906ebe3d2af7ceca5d05b9ed3e19c874b
Azrael <j.shepard.ramsay@gmail.com>
2015-11-06 16:39:33 -0500

    began working on saving record of prophecies

---
+++ Assets/ProphecyManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Fungus;
+using SimpleJSON;
+public class ProphecyManager : MonoBehaviour {
+    JSONNode saveData = new JSONClass();
+    
+    public Flowchart first_sacrifice;
+
+    void Start() {
+        saveData["newString"] = first_sacrifice.GetStringVariable("endingKey");
+    }
+    public void saveToFile(string fileName) {
+
+
+    }
+}
